a15adcf26d294cf7b4b803fe66a2c800646eca36
Yug-More <yugmore20@gmail.com>
2025-11-22 20:56:43 -0800

    fixed logo placement

--- parallel-frontend/src/components/Sidebar.jsx
+++ parallel-frontend/src/components/Sidebar.jsx
@@ -1,16 +1,20 @@
-import "./Sidebar.css";
 import ThemeToggle from "./ThemeToggle";
+import logo from "../assets/parallel-logo.svg"; 
+import "./Sidebar.css";
 
 export default function Sidebar() {
   return (
     <div className="sidebar glass">
-      <div className="logo">Parallel OS</div>
 
-      <div className="menu">
-        <div className="menu-item">Dashboard</div>
-        <div className="menu-item">Chat</div>
-        <div className="menu-item">Team</div>
-        <div className="menu-item">Code Summary</div>
+      <div className="sidebar-header">
+        <img src={logo} alt="Parallel Logo" className="sidebar-logo" />
+      </div>
+
+      <div className="sidebar-links">
+        <p>Dashboard</p>
+        <p>Chat</p>
+        <p>Team</p>
+        <p>Code</p>
       </div>
 
       <ThemeToggle />
